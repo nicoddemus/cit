c672857c8bb591df30860ee1c598b605fc731972
Bruno Oliveira <nicoddemus@gmail.com>
2013-05-30 22:13:39 -0300

    installation no longer tries to do everything automatically
    - Keep getting into roadblocks because cit --install reads information
      from stdin, and that doesn't work when trying to pipe the script 
      into python from curl or wget.

---
+++ install.py
@@ -0,0 +1,14 @@
+'''
+Simple install script to update submodules and execute initial configuration.
+
+Usage::
+    python cit_install.py
+'''
+import subprocess
+
+print '=' * 60
+print 'Fetching submodules'
+print '=' * 60
+subprocess.check_call('git submodule init', shell=True)
+subprocess.check_call('git submodule update', shell=True)
+subprocess.check_call('python cit.py --install', shell=True)

9f6fe40596a7255d39cb0569f93aee3280167863
Marcos Damiani <damiani@esss.com.br>
2013-07-16 08:39:54 -0300

    Code formatting.

--- cit.py
+++ cit.py
@@ -9,11 +9,11 @@ def configure_submodules_path():
     any other imports.
     '''
     import sys, os
-    
+
     directory = os.path.abspath(os.path.dirname(__file__))
     sys.path.insert(0, os.path.join(directory, 'jenkinsapi'))
     sys.path.insert(0, os.path.join(directory, 'pyyaml', 'lib'))
-    
+
 configure_submodules_path()
 
 #===================================================================================================
@@ -34,7 +34,7 @@ import urllib2
 # ------------
 #
 # Functions below handle the actual "meat" of cit's commands.
-#  
+#
 #===================================================================================================
 
 #===================================================================================================
@@ -48,77 +48,77 @@ def create_feature_branch_job(jenkins, job_name, new_job_name, branch, user_emai
         job = jenkins.copy_job(job_name, new_job_name)
     else:
         status = 'UPDATED'
-        
+
     print '%s => %s (%s)' % (job_name, new_job_name, status)
-    
+
     original_job = jenkins.get_job(job_name)
     tree = ET.fromstring(original_job.get_config())
-    
+
     branch_elements = list(tree.findall('.//hudson.plugins.git.BranchSpec/name'))
     if len(branch_elements) > 0:
         branch_elements[0].text = branch
     else:
         print '  warning: Could not find any branch spec to replace!'
-    
+
     # If displayName exists adds the feature branch name to it.
     display_name_elem = tree.find('./displayName')
     if display_name_elem is not None:
         display_name_elem.text = '%s (%s)' % (display_name_elem.text, branch)
-    
+
     recipient_elements = list(tree.findall('.//hudson.tasks.Mailer/recipients'))
     if len(recipient_elements) == 1:
         recipient_element = recipient_elements[0]
         recipient_element.text = user_email
-        
+
     # remove properties from the build so we can use "start" to start-up jobs
     properties_elem = tree.find('./properties')
     if properties_elem is not None:
         for elem in properties_elem.findall('./hudson.model.ParametersDefinitionProperty'):
             properties_elem.remove(elem)
-            
+
     # add a scm poll trigger for the build with 5 min intervals
     triggers_elem = tree.find('./triggers')
     scm_trigger = ET.SubElement(triggers_elem, 'hudson.triggers.SCMTrigger')
     ET.SubElement(scm_trigger, 'spec')
     ET.SubElement(scm_trigger, 'ignorePostCommitHooks').text = 'false'
-    
+
     # remove build triggers after this job
     publishers_elem = tree.find('./publishers')
     if publishers_elem is not None:
         for elem in publishers_elem.findall('./hudson.tasks.BuildTrigger'):
             publishers_elem.remove(elem)
-             
-    job.update_config(ET.tostring(tree))
-    
-    return job
-        
 
-        
+    job.update_config(ET.tostring(tree))
+
+    return job
+
+
+
 #===================================================================================================
 # cit_add
 #===================================================================================================
 def cit_add(branch, global_config):
     cit_file_name, job_config = load_cit_local_config(os.getcwd())
-    
+
     if branch is None:
         branch = get_git_branch(cit_file_name)
-    
+
     jenkins_url = global_config['jenkins']['url']
     jenkins = Jenkins(jenkins_url)
     for job_name, new_job_name in get_configured_jobs(branch, job_config):
         user_name, user_email = get_git_user(cit_file_name)
         create_feature_branch_job(jenkins, job_name, new_job_name, branch, user_email)
-        
-        
+
+
 #===================================================================================================
 # cit_rm
 #===================================================================================================
 def cit_rm(branch, global_config):
     cit_file_name, job_config = load_cit_local_config(os.getcwd())
-    
+
     if branch is None:
         branch = get_git_branch(cit_file_name)
-    
+
     jenkins_url = global_config['jenkins']['url']
     jenkins = Jenkins(jenkins_url)
     for _, new_job_name in get_configured_jobs(branch, job_config):
@@ -127,19 +127,19 @@ def cit_rm(branch, global_config):
             print new_job_name, '(REMOVED)'
         else:
             print new_job_name, '(NOT FOUND)'
-        
+
 #===================================================================================================
 # cit_start
 #===================================================================================================
-def cit_start(branch, global_config):        
+def cit_start(branch, global_config):
     cit_file_name, job_config = load_cit_local_config(os.getcwd())
-    
+
     if branch is None:
         branch = get_git_branch(cit_file_name)
-    
+
     jenkins_url = global_config['jenkins']['url']
     jenkins = Jenkins(jenkins_url)
-    
+
     for _, new_job_name in get_configured_jobs(branch, job_config):
         if jenkins.has_job(new_job_name):
             job = jenkins.get_job(new_job_name)
@@ -151,14 +151,14 @@ def cit_start(branch, global_config):
         else:
             status = '(NOT FOUND)'
         print new_job_name, status
- 
-        
+
+
 #===================================================================================================
 # git helpers
 # -----------
 #
-# Git-related helper functions to extract user name, current branch, etc. 
-#  
+# Git-related helper functions to extract user name, current branch, etc.
+#
 #===================================================================================================
 
 
@@ -170,12 +170,12 @@ def get_git_user(cit_file_name):
         user_name = check_output('git config --get user.name', shell=True).strip()
         user_email = check_output('git config --get user.email', shell=True).strip()
         return user_name, user_email
-        
-        
+
+
 #===================================================================================================
 # get_git_branch
 #===================================================================================================
-def get_git_branch(cit_file_name):        
+def get_git_branch(cit_file_name):
     with chdir(cit_file_name):
         return check_output('git rev-parse --abbrev-ref HEAD', shell=True).strip()
 
@@ -184,8 +184,8 @@ def get_git_branch(cit_file_name):
 # cit configuration
 # -----------------
 #
-# Functions and commands that deal with cit's configuration. 
-#  
+# Functions and commands that deal with cit's configuration.
+#
 #===================================================================================================
 
 #===================================================================================================
@@ -193,32 +193,32 @@ def get_git_branch(cit_file_name):
 #===================================================================================================
 def cit_init(global_config, stdin):
     cit_file_name, config = load_cit_local_config(os.getcwd())
-    
+
     print 'Configuring jobs for feature branches: %s' % cit_file_name
-    print 
-    
+    print
+
     updated = 0
     while True:
         sys.stdout.write('Source job (empty to exit):      ')
         source_job = stdin.readline().strip()
         if not source_job:
             break
-        
+
         sys.stdout.write('Feature job (shh, use $name):    ')
         fb_job = stdin.readline().strip()
         if not fb_job:
             break
-        
+
         fb_data = {
             'source-job' : source_job,
-            'feature-branch-job' : fb_job, 
+            'feature-branch-job' : fb_job,
         }
         config.setdefault('jobs', []).append(fb_data)
         updated += 1
         print 'Done! Next?'
         print
-        
-    print 
+
+    print
     if updated:
         f = file(cit_file_name, 'w')
         f.write(yaml.dump(config, default_flow_style=False))
@@ -226,8 +226,8 @@ def cit_init(global_config, stdin):
         print 'Done! Configured %d job(s)!' % updated
     else:
         print 'Abort? Okaay.'
-    
-    
+
+
 #===================================================================================================
 # cit_install
 #===================================================================================================
@@ -239,7 +239,7 @@ def cit_install(global_config_file, stdin):
     jenkins_url = stdin.readline().strip()
     if not jenkins_url.startswith('http'):
         jenkins_url = 'http://' + jenkins_url
-        
+
     print
     print 'Checking Jenkins server...',
     try:
@@ -248,28 +248,28 @@ def cit_install(global_config_file, stdin):
         print 'ERROR (%s)' % e
     else:
         print 'OK'
-    
+
     config = {
     'jenkins' : {
         'url' : jenkins_url,
         }
     }
-    
+
     f = file(global_config_file, 'w')
     f.write(yaml.dump(config, default_flow_style=False))
     f.close()
-    
+
 
 #===================================================================================================
 # get_configured_jobs
 #===================================================================================================
-def get_configured_jobs(branch, job_config):  
+def get_configured_jobs(branch, job_config):
     for job_config in job_config['jobs']:
-        job_name = job_config['source-job'] 
+        job_name = job_config['source-job']
         new_job_name = job_config['feature-branch-job'].replace('$name', branch)
         yield job_name, new_job_name
-        
-        
+
+
 #===================================================================================================
 # load_cit_local_config
 #===================================================================================================
@@ -281,18 +281,18 @@ def load_cit_local_config(from_dir):
         if os.path.isdir(gitdir):
             break
         from_dir = os.path.dirname(from_dir)
-        
-        tries += 1    
+
+        tries += 1
         if tries >= max_tries:
             raise RuntimeError('could not find .git directory')
-        
+
     cit_file_name = os.path.join(from_dir, '.cit.yaml')
-    
+
     config = {}
     if os.path.isfile(cit_file_name):
         loaded_config = yaml.load(file(cit_file_name).read()) or {}
         config.update(loaded_config)
-        
+
     return cit_file_name, config
 
 
@@ -303,25 +303,25 @@ def main(argv, global_config_file=None, stdin=None):
     # default values
     if global_config_file is None:
         global_config_file = os.path.join(os.path.dirname(__file__), 'citconfig.yaml')
-        
+
     if stdin is None:
         stdin = sys.stdin
-        
+
     # --install option: used to initialize configuration
     if '--install' in argv:
         cit_install(global_config_file, stdin)
         return RETURN_CODE_OK
-    
+
     # read global config
     if not os.path.isfile(global_config_file):
         print >> sys.stderr, 'could not find cit config file at: %s' % global_config_file
         return RETURN_CODE_CONFIG_NOT_FOUND
-        
-    global_config = yaml.load(file(global_config_file).read()) 
-    
+
+    global_config = yaml.load(file(global_config_file).read())
+
     # command dispatch
     if len(argv) <= 1:
-        print_help() 
+        print_help()
         return RETURN_CODE_OK
     elif argv[1] == 'init':
         cit_init(global_config, stdin)
@@ -348,36 +348,36 @@ def main(argv, global_config_file=None, stdin=None):
 
 # Error Codes --------------------------------------------------------------------------------------
 
-RETURN_CODE_OK = 0 
+RETURN_CODE_OK = 0
 RETURN_CODE_UNKNOWN_COMMAND = 2
-RETURN_CODE_CONFIG_NOT_FOUND = 3 
+RETURN_CODE_CONFIG_NOT_FOUND = 3
 
 
 #===================================================================================================
 # print_help
 #===================================================================================================
 def print_help():
-    print 'Commands:'    
-    print     
+    print 'Commands:'
+    print
     print '    init                   configures jobs for feature branches for this git repo'
     print '    add [BRANCH]           add a new feature branch job to Jenkins'
     print '    start [BRANCH]         starts a new build for the given feature branch'
     print '    rm [BRANCH]            removes job for feature branches given'
-    print    
+    print
 
 
 #===================================================================================================
 # general utilities
 # -----------------
-# 
-# General utilities that didn't fit in any other category. 
+#
+# General utilities that didn't fit in any other category.
 #
 #===================================================================================================
 
 #===================================================================================================
 # chdir
 #===================================================================================================
-@contextlib.contextmanager        
+@contextlib.contextmanager
 def chdir(cwd):
     old_cwd = os.getcwd()
     if os.path.isfile(cwd):
@@ -385,8 +385,8 @@ def chdir(cwd):
     os.chdir(cwd)
     yield
     os.chdir(old_cwd)
-    
-    
+
+
 #===================================================================================================
 # check_output
 #===================================================================================================
@@ -408,4 +408,4 @@ def check_output(*args, **kwargs):
 # main
 #===================================================================================================
 if __name__ == '__main__':
-    sys.exit(main(sys.argv)) 
+    sys.exit(main(sys.argv))

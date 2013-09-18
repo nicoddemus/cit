0687f4a82c1bc9c5a8380121b146a53b772836dc
Bruno Oliveira <nicoddemus@gmail.com>
2013-09-18 20:58:13 -0300

    adding test for "fb.rm" command
    - grouped fb.* commands into a test class for easier
      sharing of code and pytest decorators (this is a WIP)
    - also updated test_add to proper name and docs
    
    this fixes #1.

--- test_cit.py
+++ test_cit.py
@@ -19,16 +19,28 @@ JOB_TEST_PREFIX = 'cit-test-job-'
 # jenkins
 #===================================================================================================
 @pytest.fixture
-def tmp_job_name(request):
+def tmp_job_name(request, tmpdir):
     config = file(os.path.join(os.path.dirname(__file__), 'test_config.xml')).read()
     
     jenkins = Jenkins(JENKINS_URL)
     
+    # create job using base config
     hasher = hashlib.sha1(str(time.time()))
     job_name = '%s%s' % (JOB_TEST_PREFIX, hasher.hexdigest())
     
     job = jenkins.create_job(job_name, config)
     job.update_config(config) 
+    
+    # configure local cit information using this job as base
+    cit_config = {
+        'jobs' : [{
+            'source-job' : job_name,
+            'feature-branch-job' : job_name + '-$name',
+        }],
+    }
+    cit_config_file = str(tmpdir.join('.cit.yaml'))
+    yaml.dump(cit_config, file(cit_config_file, 'w'))
+    
     
     def delete_test_jobs():
         '''
@@ -77,76 +89,103 @@ def global_config_file(tmpdir):
     
 
 #===================================================================================================
-# test_add
+# TestFeatureBranchCommands
 #===================================================================================================
 @pytest.mark.skipif('not config.option.jenkins_available')
 @pytest.mark.usefixtures('change_cwd')
-@pytest.mark.parametrize('branch', ['new-feature', None])
-def test_add(tmp_job_name, tmpdir, global_config_file, branch):
+class TestFeatureBranchCommands(object):
     '''
-    test command "add"
-    
-    :param branch: 
-        parametrized to test adding passing a branch name in the command line and without
-        (which means "use current branch as branch name") 
+    Class that groups all tests for feature branch commands that also require a 
+    jenkins instance to be executed.
     '''
-    cit_config = {
-        'jobs' : [{
-            'source-job' : tmp_job_name,
-            'feature-branch-job' : tmp_job_name + '-$name',
-        }],
-    }
-    cit_config_file = str(tmpdir.join('.cit.yaml'))
-    yaml.dump(cit_config, file(cit_config_file, 'w'))
-    
-    with mock.patch('cit.get_git_branch', autospec=True) as mock_get_git_branch:
-        mock_get_git_branch.return_value = 'new-feature'
+
+    @pytest.mark.parametrize('branch', ['new-feature', None])
+    def test_fb_add(self, tmp_job_name, global_config_file, branch):
+        '''
+        test "fb.add" command
         
-        with mock.patch('cit.get_git_user', autospec=True) as mock_get_git_user:
-            mock_get_git_user.return_value = ('anonymous', 'anonymous@somewhere.com')
+        :param branch: 
+            parametrized to test adding passing a branch name in the command line and without
+            (which means "use current branch as branch name") 
+        '''
+        with mock.patch('cit.get_git_branch', autospec=True) as mock_get_git_branch:
+            mock_get_git_branch.return_value = 'new-feature'
             
+            with mock.patch('cit.get_git_user', autospec=True) as mock_get_git_user:
+                mock_get_git_user.return_value = ('anonymous', 'anonymous@somewhere.com')
+                
+                with mock.patch('cit.get_global_config_file', autospec=True) as mock_get_global_config_file:
+                    mock_get_global_config_file.return_value = str(global_config_file)
+                    argv = ['fb.add']
+                    if branch:
+                        argv.append(branch)
+                    assert cit.app.main(argv) is None
+        
+        branch = 'new-feature'
+        
+        jenkins = Jenkins(JENKINS_URL)
+        new_job_name = tmp_job_name + '-' + branch
+        assert jenkins.has_job(new_job_name), "no job %s found. available: %s" % (new_job_name, jenkins.get_jobs_list())
+        
+        config_xml = jenkins.get_job(new_job_name).get_config()
+        
+        # ensure we configured branch correctly
+        config = ET.fromstring(config_xml)
+        branches_elements = list(config.findall('.//branches'))
+        assert len(branches_elements) == 1
+        branches_elem = branches_elements[0]
+        name_elem = branches_elem.findall('hudson.plugins.git.BranchSpec/name')[0]    
+        assert name_elem.text == branch
+        
+        # ensure we have updated the display name with the feature branch's name
+        display_name_elements = list(config.findall('.//displayName'))
+        assert len(display_name_elements) == 1
+        display_name_element = display_name_elements[0]
+        assert display_name_element.text == '%s SS win32' % branch
+        
+        # ensure we have set the user email recipient
+        recipient_elements = list(config.findall('.//hudson.tasks.Mailer/recipients'))
+        assert len(recipient_elements) == 1
+        recipient_element = recipient_elements[0]
+        assert recipient_element.text == 'anonymous@somewhere.com'
+        
+        # ensure we don't have build parameters anymore
+        params = list(config.findall('.//hudson.model.ParametersDefinitionProperty'))
+        assert len(params) == 0
+    
+        # ensure no build is triggered after the job
+        build_triggers = list(config.findall('.//hudson.tasks.BuildTrigger'))
+        assert len(build_triggers) == 0
+        
+        
+    @pytest.mark.parametrize('branch', ['new-feature', None])
+    def test_fb_rm(self, tmp_job_name, global_config_file, branch):
+        '''
+        test "fb.rm" command
+        
+        :param branch: 
+            parametrized to test removing passing a branch name in the command line and without
+            (which means "use current branch as branch name") 
+        '''
+        jenkins = Jenkins(JENKINS_URL)
+        new_job_name = tmp_job_name + '-new-feature'
+        jenkins.copy_job(tmp_job_name, new_job_name)
+        
+        jenkins = Jenkins(JENKINS_URL)
+        assert jenkins.has_job(new_job_name), "no job %s found. available: %s" % (new_job_name, jenkins.get_jobs_list())
+        
+        with mock.patch('cit.get_git_branch', autospec=True) as mock_get_git_branch:
+            mock_get_git_branch.return_value = 'new-feature'
+                
             with mock.patch('cit.get_global_config_file', autospec=True) as mock_get_global_config_file:
                 mock_get_global_config_file.return_value = str(global_config_file)
-                argv = ['fb.add']
+                argv = ['fb.rm']
                 if branch:
                     argv.append(branch)
                 assert cit.app.main(argv) is None
-    
-    branch = 'new-feature'
-    
-    jenkins = Jenkins(JENKINS_URL)
-    new_job_name = tmp_job_name + '-' + branch
-    assert jenkins.has_job(new_job_name), "no job %s found. available: %s" % (new_job_name, jenkins.get_jobs_list())
-    
-    config_xml = jenkins.get_job(new_job_name).get_config()
-    
-    # ensure we configured branch correctly
-    config = ET.fromstring(config_xml)
-    branches_elements = list(config.findall('.//branches'))
-    assert len(branches_elements) == 1
-    branches_elem = branches_elements[0]
-    name_elem = branches_elem.findall('hudson.plugins.git.BranchSpec/name')[0]    
-    assert name_elem.text == branch
-    
-    # ensure we have updated the display name with the feature branch's name
-    display_name_elements = list(config.findall('.//displayName'))
-    assert len(display_name_elements) == 1
-    display_name_element = display_name_elements[0]
-    assert display_name_element.text == '%s SS win32' % branch
-    
-    # ensure we have set the user email recipient
-    recipient_elements = list(config.findall('.//hudson.tasks.Mailer/recipients'))
-    assert len(recipient_elements) == 1
-    recipient_element = recipient_elements[0]
-    assert recipient_element.text == 'anonymous@somewhere.com'
-    
-    # ensure we don't have build parameters anymore
-    params = list(config.findall('.//hudson.model.ParametersDefinitionProperty'))
-    assert len(params) == 0
-
-    # ensure no build is triggered after the job
-    build_triggers = list(config.findall('.//hudson.tasks.BuildTrigger'))
-    assert len(build_triggers) == 0
+        
+        jenkins = Jenkins(JENKINS_URL)
+        assert not jenkins.has_job(new_job_name), "job %s found! available: %s" % (new_job_name, jenkins.get_jobs_list())
     
     
 #===================================================================================================
@@ -213,6 +252,9 @@ def test_cit_install(tmpdir):
         contents = global_config_file.read()
         obtained = yaml.load(contents)
         assert obtained == {'jenkins' : {'url' : 'http://localhost:8080'}}     
+    
+    
+
     
     
 #===================================================================================================
